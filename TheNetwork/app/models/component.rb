47ad39e958dceae318a57738903b3f67da96946b
rnexer <rnexer@gmail.com>
2015-07-05 03:30:27 -0600

    Pagina principal de Juego X

--- TheNetwork/app/models/component.rb
+++ TheNetwork/app/models/component.rb
@@ -2,4 +2,6 @@ class Component < ActiveRecord::Base
   belongs_to :engine
   has_many :games, through: :component_in_games
   has_many :component_in_games
+  belongs_to :license
+  has_many :component_ratings
 end
