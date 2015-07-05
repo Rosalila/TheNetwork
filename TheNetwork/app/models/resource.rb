0b35a65a17ddb85dab5de34801bbdffd53172f0c
Turupawn <ahmed.hn.43@gmail.com>
2015-07-05 06:16:06 -0600

    cosas

--- TheNetwork/app/models/resource.rb
+++ TheNetwork/app/models/resource.rb
@@ -1,4 +1,7 @@
 class Resource < ActiveRecord::Base
+  belongs_to :license
+  belongs_to :category
+  has_many :resource_ratings
   mount_uploader :image, FileUploader
   mount_uploader :download, FileUploader
 end
