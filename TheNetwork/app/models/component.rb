bfd5c7e09d4e1918f2900512f5bddbcbd708fdc9
rnexer <rnexer@gmail.com>
2015-07-05 12:21:40 -0600

    Biblioteca de Components

--- TheNetwork/app/models/component.rb
+++ TheNetwork/app/models/component.rb
@@ -5,5 +5,6 @@ class Component < ActiveRecord::Base
   belongs_to :license
   has_many :component_ratings
   has_many :resources
+  has_many :component_tags
   mount_uploader :image, FileUploader
 end
